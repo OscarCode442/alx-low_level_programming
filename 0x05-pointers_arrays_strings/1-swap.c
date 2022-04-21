7832c5890cf2aa396255ecf4027b6ad84b8c8e52
OscarCode442 <102172374+OscarCode442@users.noreply.github.com>
2022-04-21 20:19:19 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * swap_int - swap variable values
- * @a: pointer 1
- * @b: pointer 2
- * Return: void
-*/
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
 
 void swap_int(int *a, int *b)
 {
-*a += *b;
-*b = *a - *b;
-*a = *a - *b;
+	int c = *a;
+	*a = *b;
+	*b = c;
 }
